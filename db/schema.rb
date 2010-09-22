3506e873edaa2210fc2fba9b5c249880a43b5336
makestory <makestory@zoho.com>
2010-09-22 15:11:08 +0800

    add auto-migration plugin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100922013916) do
+ActiveRecord::Schema.define do
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
