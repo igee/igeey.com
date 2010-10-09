d605eb5199afd1f228a1dd23162afc74cf2e81c0
makestory <makestory@zoho.com>
2010-10-09 19:34:16 +0800

    change behavior of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,17 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-users = User.create([
-                      {:login => 'admin',
-                       :email => 'admin@igee.org',
-                       :password => "password",
-                       :password_confirmation => "password"}
-                  ])
+User.new(:login => 'admin',
+         :email => 'admin@igee.org',
+         :password => "password",
+         :password_confirmation => "password"
+        ).save
 
-geos = Geo.create([
-                    { :name => '北京',:latitude => '39.9046670',:longitude => '116.4081980',:zoom_level => 9 },
-                    { :name => '天津',:latitude => '39.1208760',:longitude => '117.2150300',:zoom_level => 9 }
-                    ])
 
-actions = Action.create([
-                    { :name => '公益旅行',:for_time => true},
-                    { :name => '捐款',:for_amount => true },
-                    { :name => '捐物',:for_goods => true },
-                    ])
+Geo.new(:name => '北京',:latitude => '39.9046670',:longitude => '116.4081980',:zoom_level => 9).save
+Geo.new(:name => '天津',:latitude => '39.1208760',:longitude => '117.2150300',:zoom_level => 9).save
+                    
+
+Action.new(:name => '志愿服务',:for_time => true).save
+Action.new(:name => '捐款',:for_amount => true).save
+Action.new(:name => '捐物',:for_goods => true).save
