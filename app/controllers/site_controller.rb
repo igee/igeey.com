649de95bf3e1439912f296eaa9ee7d8b26a71c63
makestory <makestory@zoho.com>
2010-10-14 14:53:38 +0800

    set limit on requirement list

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
   def index
-    @requirements = Requirement.all
+    @requirements = Requirement.limit(8)
     @venues = Venue.all
     @plan = Plan.new     #can create new plan on this page 
   end
