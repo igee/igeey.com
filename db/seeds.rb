1f7bb31e3541ca8b07fd5e5da01e895c3e346c4b
makestory <makestory@zoho.com>
2010-10-20 18:34:24 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,8 @@ Geo.new(:name => '重庆',:latitude => '29.5626860',:longitude => '106.5512340',
 Venue.new(:name => '太阳村',:geo_id => 1,:category => '1',:creator_id => 1,:latitude => "40.21106465394306",:longitude => "116.53745901684572",:intro => "张淑琴女士于1996年创办的以无偿抚养服刑人员未成年子女为目标的非营利性慈善机构。").save
 Venue.new(:name => '某流浪狗收容站',:geo_id => 1,:category => '3',:creator_id => 1,:latitude => "39.91612252026889",:longitude => "116.49694693188478",:intro => "社区里的流浪狗收容站，有20多只流浪狗在这里生活。").save
 Venue.new(:name => '大屯里小区',:geo_id => 1,:category => '3',:creator_id => 2,:latitude => "40.00904440587653",:longitude => "116.41574621200562",:intro => "爱聚办公室所在，这里卫生环境不太好。").save
+Venue.new(:name => '温江养老院',:geo_id => 5,:category => '3',:creator_id => 2,:latitude => "30.701695962426008",:longitude => "103.83110046386719",:intro => "一家老人很多的养老院。").save
+Venue.new(:name => '春蕾爱心留守儿童之家',:geo_id => 5,:category => '1',:creator_id => 1,:latitude => "39.842285409843065",:longitude => "116.3671875",:intro => "春蕾爱心留守儿童之家是春蕾爱心协会免费为广元300多名留守儿童和孤残儿童提供的一个学习场所，是孩子们周末和节假日学习和生活的家园，这里为孩子们提供的一切吃住和学习用品都依靠社会好心的朋友捐助。").save
 
 Action.new(:name => '志愿服务',:for_what => 'time').save
 Action.new(:name => '捐款',:for_what => 'money').save
