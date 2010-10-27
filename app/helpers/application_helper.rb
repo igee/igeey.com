63da76347336819ece794d8775693616ab3b810f
makestory <makestory@zoho.com>
2010-10-27 22:11:35 +0800

    modify oauth_checkbox_tag style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,6 @@ module ApplicationHelper
     html = ""
     html << "#{form.check_box :sync_to_sina ,:disabled => (current_user.sina? ? false : true)} 同步到新浪微博 "
     html << "#{form.check_box :sync_to_douban ,:disabled => (current_user.douban? ? false : true)} 同步到豆瓣 "
+    html << "　#{link_to '无法同步？快去设置吧',setting_path,:target => '_blank',:class => 'with_explain',:title => '点击页面右上角的用户设置，连接你的社区帐号，把你公益行动分享给你的朋友。'}" unless current_user.sina? || current_user.douban?
   end
 end
