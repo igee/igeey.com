dfe6ba01dec9b855f796169f094282a66e7c6a13
makestory <makestory@zoho.com>
2010-11-22 14:38:56 +0800

    text update

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
 class SiteController < ApplicationController
-  before_filter :login_required, :except=> [:index]
+  before_filter :login_required, :except=> [:index,:faq,:guide,:about,:report]
   
   def index
     # group callings,plans and records to list
