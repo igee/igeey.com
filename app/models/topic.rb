5601ba07f4e2c4569811493097e4aa72377ee323
makestory <makestory@zoho.com>
2010-11-26 15:16:50 +0800

    topics order by last_replied_at

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,5 +4,5 @@ class Topic < ActiveRecord::Base
   belongs_to :last_replied_user,:class_name => 'User' ,:foreign_key => :last_replied_user_id
   
   has_many   :comments, :as => :commentable,    :dependent => :destroy
-  default_scope :order => 'created_at DESC'
+  default_scope :order => 'last_replied_at DESC created_at DESC'
 end
