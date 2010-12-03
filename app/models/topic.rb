15078855c219dccb167847fa1c5a0df51a0a9377
makestory <makestory@zoho.com>
2010-12-03 21:34:39 +0800

    add validation on topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,8 +2,11 @@ class Topic < ActiveRecord::Base
   belongs_to :user
   belongs_to :venue
   belongs_to :last_replied_user,:class_name => 'User' ,:foreign_key => :last_replied_user_id
-  has_many   :follows,:as => :followable,:dependent => :destroy
-  
+  has_many   :follows,:as => :followable,:dependent => :destroy  
   has_many   :comments, :as => :commentable,    :dependent => :destroy
+  
   default_scope :order => 'last_replied_at DESC'
+  
+  validate  :title, :presence  => true
+  validate  :content, :presence  => true
 end
