cfda843642e3adba3b083f804d71fbbea2250434
makestory <makestory@zoho.com>
2010-12-07 17:58:56 +0800

    chart update

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -4,5 +4,11 @@ class ChartController < ApplicationController
         eval("@#{model.name.downcase}s_per_day = (0..30).map{|a| model.where(:created_at => (a + 1).days.ago..a.days.ago).size}.reverse")
         eval("@#{model.name.downcase}s_total = #{model.all.size}")
     end
+    @douban_syncs = Sync.where(:douban => true).size
+    @sina_syncs = Sync.where(:sina => true).size
+    @goods_records = Record.where(:action_id => 3).size
+    @time_records = Record.where(:action_id => 1).size
+    @directly_plans = Plan.where(:parent_id => nil).size
+    @follow_plans = Plan.where("parent_id is not null").size
   end
 end
