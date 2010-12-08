5a34a70663496d37829d779b439ed0f460231a06
makestory <makestory@zoho.com>
2010-12-08 23:22:12 +0800

    update calling text

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -45,7 +45,7 @@
       $(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
       return false;
     });
-    var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/g
+    var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-|!)+)/g
     $('pre').each(function(index){$(this).html($(this).html().replace(reg, "<a href='$1$2' target='_blank' rel='nofollow'>$1$2</a>"))})
   })
   
