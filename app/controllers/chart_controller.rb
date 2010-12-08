f983c570bb751d51f5cc379cdc77132494273761
makestory <makestory@zoho.com>
2010-12-08 10:59:48 +0800

    update timezone

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,7 +1,7 @@
 class ChartController < ApplicationController
   def show
     [Venue,User,Plan,Record,Calling,Sync,Comment,Photo,Follow,Topic].each do |model|
-        eval("@#{model.name.downcase}s_per_day = (0..30).map{|a| model.where(:created_at => (a + 1).days.ago..a.days.ago).size}.reverse")
+        eval("@#{model.name.downcase}s_per_day = (0..30).map{|a| model.where(:created_at => (a + 1).days.ago.beginning_of_day..(a - 1).days.ago.beginning_of_day).size}.reverse")
         eval("@#{model.name.downcase}s_total = #{model.all.size}")
     end
     @douban_syncs = Sync.where(:douban => true).size
