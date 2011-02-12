007a87f5a2e8eb6d868a35cec04cec8423791b2c
makestory <makestory@zoho.com>
2011-02-12 09:54:31 +0800

    fix bugs

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -2,6 +2,8 @@ class CheckinsController < ApplicationController
   before_filter :login_required
   def create
     @checkin = current_user.checkins.build(params[:checkin])
+    @checkin.last_replied_at = Time.now
+    @checkin.last_replied_user_id = @checkin.user_id
     @checkin.save
     redirect_to @checkin.venue
   end
