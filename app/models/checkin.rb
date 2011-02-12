de835f34315ebb43e1d47d1134d0a8486dab2f3a
makestory <makestory@zoho.com>
2011-02-12 09:18:58 +0800

    reply_reply

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -3,4 +3,6 @@ class Checkin < ActiveRecord::Base
   belongs_to :venue
 
   has_many   :comments, :as => :commentable,    :dependent => :destroy
+  
+  default_scope :order => 'created_at DESC'
 end
