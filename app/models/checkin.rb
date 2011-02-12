62d08af15539e86731594b8113a284b5e6c2406f
makestory <makestory@zoho.com>
2011-02-12 09:48:26 +0800

    words counter validation

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -5,4 +5,6 @@ class Checkin < ActiveRecord::Base
   has_many   :comments, :as => :commentable,    :dependent => :destroy
   
   default_scope :order => 'last_replied_at DESC'
+  
+  validates :content,:length => { :within => 1..140,:message => '限制字数在140字以内' }
 end
