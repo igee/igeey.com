a2ca7e932dc62e6b539983204d69042c5d904f7c
makestory <makestory@zoho.com>
2011-03-08 08:56:08 +0800

    js bind fix for ajax loading

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user,:counter_cache => true
   belongs_to :commentable, :polymorphic => true,:counter_cache => true
   
+  default_scope :order => 'created_at asc'
+  
   validates :content,:length => {:within => 1..1000}
 end
