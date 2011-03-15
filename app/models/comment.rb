67fb5f983fc0d6dbb452755caf25560099cb4446
makestory <makestory@zoho.com>
2011-03-15 10:28:45 +0800

    ajax load more reply update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true,:counter_cache => true
   
   default_scope :order => 'created_at asc'
-  scope :last_five,:order=> 'created_at desc',:limit => 5
+  scope :last_three,:order=> 'created_at desc',:limit => 3
   
   validates :content,:length => {:within => 1..1000}
 end
