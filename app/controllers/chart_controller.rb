bf4a69fc916b49720a8d3c4d2bbb619b51579deb
makestory <makestory@zoho.com>
2011-03-15 16:42:24 +0800

    chart update

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,6 +1,6 @@
 class ChartController < ApplicationController
   def show
-    [Venue,User,Plan,Record,Calling,Sync,Comment,Photo,Follow,Topic].each do |model|
+    [Venue,User,Saying,Plan,Record,Calling,Sync,Comment,Photo,Follow,Topic].each do |model|
         eval("@#{model.name.downcase}s_per_day = (0..30).map{|a| model.where(:created_at => a.days.ago.beginning_of_day..(a - 1).days.ago.beginning_of_day).size}.reverse")
         eval("@#{model.name.downcase}s_total = #{model.all.size}")
     end
