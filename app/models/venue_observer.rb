46ff0df849db4a0c39e612f66dcdc3546e963fd2
makestory <makestory@zoho.com>
2011-03-22 16:59:40 +0800

    auto follow venue on creating venue

--- app/models/venue_observer.rb
+++ app/models/venue_observer.rb
@@ -2,5 +2,6 @@ class VenueObserver < ActiveRecord::Observer
   def after_create(venue)
     venue.class.generate_json
     venue.creator.check_badge_condition_on('venues_count')
+    Follow.create(:user_id => venue.creator_id,:followable_id => venue.id,:followable_type => 'Venue')
   end
 end
