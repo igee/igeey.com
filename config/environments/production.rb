a20d7a2a4eb2e246ec038af1162ff0bcb8d9fc86
makestory <makestory@zoho.com>
2011-03-24 13:49:07 +0800

    fix css bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Igee::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
