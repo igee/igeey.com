38d8019cbe42c36008729145964a31f00dbf89ee
makestory <makestory@zoho.com>
2011-03-25 14:56:30 +0800

    fix comment_observer bug

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -1,7 +1,7 @@
 class CommentObserver < ActiveRecord::Observer
   def after_create(comment)
     @commentable = comment.commentable
-    (@commentable.comments - [comment]).map{|a| a.update_attributes(:has_new_comment => true)}
+    @commentable.comments.where("user_id != #{comment.user_id}").map{|a| a.update_attributes(:has_new_comment => true)}
     @commentable.update_attribute(:has_new_comment ,true) unless @commentable.user_id == comment.user_id
     @commentable.update_attributes(:last_replied_user_id => comment.user.id,:last_replied_at => Time.now)
   end
