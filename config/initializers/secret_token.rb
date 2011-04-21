efe1b21875a341a8bf0160b59228d68e538083ef
makestory <makestory@zoho.com>
2011-04-22 00:18:19 +0800

    修改网站模块名

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Igee::Application.config.secret_token = APP_CONFIG['secret_token'] || 'f229c299eb2c7b64b6e65ef6df256a42138aec2c7f2bbe8fba6a10435944c5c32f287d173a4f208a4085d866f27f9241c83e076872c62242b594910e37837883'
+Igeey::Application.config.secret_token = APP_CONFIG['secret_token'] || 'f229c299eb2c7b64b6e65ef6df256a42138aec2c7f2bbe8fba6a10435944c5c32f287d173a4f208a4085d866f27f9241c83e076872c62242b594910e37837883'
