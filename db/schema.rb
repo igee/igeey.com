f0019559140c08c12a4c9094976061a3756f33ad
makestory <makestory@zoho.com>
2011-04-26 19:22:58 +0800

    增加行动

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,22 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define() do
-
+  
   create_table "actions", :force => true do |t|
     t.string  :name,        :limit => 40
-    t.string  :slug,        :limit => 40
-    t.string  :for_what,    :limit => 40
-    t.boolean :is_callable, :default => true
     t.text    :intro
-    t.text    :step_by_step
   end
 
+  create_table "answers" do |t|
+    t.integer    :user_id
+    t.integer    :question_id
+    t.text       :content
+    t.integer    :last_replied_user_id
+    t.datetime   :last_replied_at
+    t.integer    :comments_count,   :default => 0
+    t.timestamps
+  end
+  
   create_table "badges", :force => true do |t|
     t.string  :name,             :limit => 40
     t.string  :slug,             :limit => 40
@@ -28,13 +34,6 @@ ActiveRecord::Schema.define() do
     t.integer :condition_number
     t.string  :cover_file_name
     t.text    :intro
-  end
-  
-  create_table "grants", :force => true do |t|
-    t.integer  :user_id
-    t.integer  :badge_id
-    t.boolean  :unread,     :default => true
-    t.timestamps
   end
   
   create_table "comments", :force => true do |t|
@@ -45,10 +44,33 @@ ActiveRecord::Schema.define() do
     t.timestamps
   end
   
-  create_table "votes", :force => true do |t|
-    t.integer  :user_id
-    t.integer  :voteable_id
-    t.string   :voteable_type, :limit => 40
+  create_table "callings", :force => true do |t|
+    t.integer  :venue_id
+    t.integer  :action_id
+    t.integer  :user_id    
+    t.integer  :total_money
+    t.integer  :total_online
+    t.integer  :total_people
+    t.integer  :total_goods
+    t.string   :title,       :limit => 40   
+    t.string   :address
+    t.string   :contact
+    t.text     :detail
+    t.datetime :do_at
+    t.boolean  :close,            :default => false
+    t.integer  :follows_count,    :default => 0
+    t.string   :cached_tag_list,  :default => ''
+    t.boolean  :has_new_plan,     :default => false
+    t.integer  :comments_count,   :default => 0
+    t.datetime :last_replied_at
+    t.integer  :last_replied_user_id
+    t.timestamps
+  end  
+
+  create_table "events" do |t|
+    t.references :eventable, :polymorphic => true
+    t.integer    :user_id
+    t.integer    :venue_id
     t.timestamps
   end
 
@@ -77,7 +99,34 @@ ActiveRecord::Schema.define() do
     t.string  :latitude,   :limit => 40
     t.string  :longitude,  :limit => 40    
   end
+  
+  create_table "grants", :force => true do |t|
+    t.integer  :user_id
+    t.integer  :badge_id
+    t.boolean  :unread,     :default => true
+    t.timestamps
+  end
+  
+  create_table "notifications", :force => true do |t|
+    t.integer     :user_id
+    t.references  :notifiable,   :polymorphic => true
+    t.boolean     :unread,       :default => true
+    t.timestamps
+  end
+  
+  add_index "notifications", ["user_id"], :name => "index_notifications_on_user_id"
 
+  create_table "oauth_tokens",:force => true do |t|
+    t.integer :user_id
+    t.string  :unique_id
+    t.string  :request_key
+    t.string  :request_secret
+    t.string  :access_key
+    t.string  :access_secret
+    t.string  :site
+    t.timestamps
+  end
+  
   create_table "plans", :force => true do |t|
     t.integer  :user_id
     t.integer  :venue_id
@@ -95,7 +144,7 @@ ActiveRecord::Schema.define() do
     t.integer  :last_replied_user_id
     t.timestamps
   end
-
+  
   create_table "photos", :force => true do |t|
     t.integer  :user_id
     t.integer  :venue_id
@@ -109,6 +158,14 @@ ActiveRecord::Schema.define() do
     t.integer  :votes_count,      :default => 0
     t.datetime :last_replied_at
     t.integer  :last_replied_user_id
+    t.timestamps
+  end
+  
+  create_table "questions" do |t|
+    t.integer    :user_id
+    t.string     :title
+    t.text       :detail
+    t.integer    :answers_count,    :default => 0
     t.timestamps
   end
   
@@ -164,29 +221,13 @@ ActiveRecord::Schema.define() do
   
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
   
-  create_table "callings", :force => true do |t|
-    t.integer  :venue_id
-    t.integer  :action_id
-    t.integer  :user_id    
-    t.integer  :total_money
-    t.integer  :total_online
-    t.integer  :total_people
-    t.integer  :total_goods
-    t.string   :title,       :limit => 40   
-    t.string   :address
-    t.string   :contact
-    t.text     :detail
-    t.datetime :do_at
-    t.boolean  :close,            :default => false
-    t.integer  :follows_count,    :default => 0
-    t.string   :cached_tag_list,  :default => ''
-    t.boolean  :has_new_plan,     :default => false
-    t.integer  :comments_count,   :default => 0
-    t.datetime :last_replied_at
-    t.integer  :last_replied_user_id
+  create_table "votes", :force => true do |t|
+    t.integer  :user_id
+    t.integer  :voteable_id
+    t.string   :voteable_type, :limit => 40
     t.timestamps
-  end  
-
+  end
+  
   create_table "venues", :force => true do |t|
     t.string  :name,         :limit => 40
     t.text    :intro
@@ -211,17 +252,6 @@ ActiveRecord::Schema.define() do
     t.timestamps
   end
   
-  create_table "oauth_tokens",:force => true do |t|
-    t.integer :user_id
-    t.string  :unique_id
-    t.string  :request_key
-    t.string  :request_secret
-    t.string  :access_key
-    t.string  :access_secret
-    t.string  :site
-    t.timestamps
-  end
-  
   create_table "syncs",:force => true do |t|
     t.integer  :user_id
     t.integer  :syncable_id
@@ -229,6 +259,32 @@ ActiveRecord::Schema.define() do
     t.text     :content
     t.boolean  :sina,    :default => false
     t.boolean  :douban,  :default => false
+    t.timestamps
+  end
+  
+  create_table "sayings",:force => true do |t|
+    t.integer  :user_id
+    t.integer  :venue_id
+    t.text     :content
+    t.string   :cached_tag_list,  :default => ''
+    t.integer  :comments_count,   :default => 0
+    t.datetime :last_replied_at
+    t.integer  :last_replied_user_id
+    t.timestamps
+  end
+
+  create_table "tags" do |t|
+    t.string   :name
+    t.text     :intro
+    t.integer  :taggings_count,   :default => 0
+    t.timestamps
+  end
+
+  create_table "taggings" do |t|
+    t.references :tag
+    t.references :taggable, :polymorphic => true
+    t.integer    :user_id
+    t.integer    :venue_id
     t.timestamps
   end
   
@@ -246,66 +302,6 @@ ActiveRecord::Schema.define() do
     t.timestamps
   end
   
-  create_table "sayings",:force => true do |t|
-    t.integer  :user_id
-    t.integer  :venue_id
-    t.text     :content
-    t.string   :cached_tag_list,  :default => ''
-    t.integer  :comments_count,   :default => 0
-    t.datetime :last_replied_at
-    t.integer  :last_replied_user_id
-    t.timestamps
-  end
-
-  create_table "notifications", :force => true do |t|
-    t.integer     :user_id
-    t.references  :notifiable,   :polymorphic => true
-    t.boolean     :unread,       :default => true
-    t.timestamps
-  end
-  
-  add_index "notifications", ["user_id"], :name => "index_notifications_on_user_id"
-
-  create_table "tags" do |t|
-    t.string   :name
-    t.text     :intro
-    t.integer  :taggings_count,   :default => 0
-    t.timestamps
-  end
-
-  create_table "taggings" do |t|
-    t.references :tag
-    t.references :taggable, :polymorphic => true
-    t.integer    :user_id
-    t.integer    :venue_id
-    t.timestamps
-  end
-
-  create_table "events" do |t|
-    t.references :eventable, :polymorphic => true
-    t.integer    :user_id
-    t.integer    :venue_id
-    t.timestamps
-  end
-
-  create_table "questions" do |t|
-    t.integer    :user_id
-    t.string     :title
-    t.text       :detail
-    t.integer    :answers_count,    :default => 0
-    t.timestamps
-  end
-
-  create_table "answers" do |t|
-    t.integer    :user_id
-    t.integer    :question_id
-    t.text       :content
-    t.integer    :last_replied_user_id
-    t.datetime   :last_replied_at
-    t.integer    :comments_count,   :default => 0
-    t.timestamps
-  end
-    
   # add_index :taggings, :tag_id
   # add_index :taggings, [:taggable_id, :taggable_type]  
 end
