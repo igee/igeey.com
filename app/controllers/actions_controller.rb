40fee4704b400a6fe20f12b5981b235173544aa8
makestory <makestory@zoho.com>
2011-04-26 15:06:06 +0800

    重新定义action

--- app/controllers/actions_controller.rb
+++
@@ -1,2 +0,0 @@
-class ActionsController < ApplicationController
-end
--- app/models/action.rb
+++ app/models/action.rb
@@ -1,5 +1,2 @@
 class Action < ActiveRecord::Base
-  validates :name,:uniqueness => true
-  validates :for_what,:inclusion => { :in => ['money','time','goods'] }
-  scope :callable,where(:is_callable => true) 
 end
