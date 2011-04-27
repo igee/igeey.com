5134f13edc53db23055d291944de73ff9c436c78
mhb0925 <muhaibolove@gmail.com>
2011-04-27 16:55:35 +0800

    Action创建的时候自动关联Tag

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,10 +1,18 @@
 class Action < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
-  
+    
   has_attached_file :cover, :styles => {:_64x64 => ["64x64#",:jpg],:_32x32 => ["32x32#",:jpg],:_24x24 => ["24x24#",:jpg]},
                             :url=>"/media/:attachment/action/:id/:style.jpg",
                             :default_style=> :_64x64,
                             :default_url=>"/defaults/:attachment/action/:style.png"
+
+  def taggings
+    self.tag.taggings
+  end
+  
+  def questions
+    self.taggings.where(:taggable_type=>"Question").map(&:taggable)
+  end
 
 end
