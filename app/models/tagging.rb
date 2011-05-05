6370aae658cdc1d6042ad2e9e5177d15562206c3
makestory <makestory@zoho.com>
2011-05-05 17:12:48 +0800

    更新标签页面和Tag的cache_count关系

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -9,7 +9,7 @@
     
     default_scope :order => 'created_at DESC'
     
-    belongs_to :tag, :counter_cache => true
+    belongs_to :tag, :counter_cache => 'taggeds_count'
     belongs_to :taggable, :polymorphic => true
     belongs_to :user
 
