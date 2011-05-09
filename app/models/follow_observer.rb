9698dec1e2e89befa5014cee8c07f963ce70a984
makestory <makestory@zoho.com>
2011-05-10 00:55:42 +0800

    修复关注提醒的Observer

--- app/models/follow_observer.rb
+++ app/models/follow_observer.rb
@@ -2,7 +2,7 @@ class FollowObserver < ActiveRecord::Observer
   def after_create(follow)
     follow.user.check_badge_condition_on('followings_count')
     if follow.followable_type.to_s == "User"
-      Notification.update([follow.followable_id], follow.user)
+      Notification.update(follow.user,follow)
     end
   end  
 end
