9653c7ccdbc3aca7164357b16284af1119710fcc
mhb0925 <muhaibolove@gmail.com>
2011-06-08 10:43:52 +0800

    plan_spec使用Factory

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -5,41 +5,47 @@ describe Plan do
     @user = makestory
     @user_1 = mhb
     
-    @task = Task.new()
-    @task.save
+    @task = Factory(:task)
   end
   describe 'validation' do
     it 'should be created by task_id' do
-      @plan = Plan.new(:user_id=>@user.id, :content=>'plan content')
+      @plan = Factory(:plan)
+      @plan.task_id = nil
       @plan.should_not be_valid
       
-      @plan = Plan.new(:user_id=>@user.id, :task_id=>@task.id, :content=>'plan content')
+      @plan.task_id = @task.id
       @plan.should be_valid
     end
     
     it 'should be created by user_id' do
-      @plan = Plan.new(:task_id=>@task.id, :content=>'plan content')
+      @plan = Factory(:plan)
+      @plan.user_id = nil
       @plan.should_not be_valid
       
-      @plan = Plan.new(:user_id=>@user.id, :task_id=>@task.id, :content=>'plan content')
+      @plan.user_id = @user.id
       @plan.should be_valid
     end
     
     it 'should be created by content' do
-      @plan = Plan.new(:user_id=>@user.id, :task_id=>@task.id)
+      @plan = Factory(:plan)
+      @plan.content = nil
       @plan.should_not be_valid
       
-      @plan = Plan.new(:user_id=>@user.id, :task_id=>@task.id, :content=>'plan content')
+      @plan.content = 'plan content'
       @plan.should be_valid
     end
     
     it 'should only be one plan to the same user with a task' do
-      @plan = Plan.new(:user_id=>@user.id, :task_id=>@task.id, :content=>'plan content')
+      @plan = Factory(:plan)
+      @plan.task_id = @task.id
+      @plan.user_id = @user.id
       @plan.should be_valid
       @plan.save
       
-      @plan_1 = Plan.new(:user_id=>@user.id, task_id=>@task.id, :content=>'plan content 2')
-      @plan_1.should_not be valid
+      @plan_1 = Factory(:plan)
+      @plan.task_id = @task.id
+      @plan.user_id = @user.id
+      @plan_1.should_not be_valid
     end
     
     it 'should be plan multiple users with a task' do
