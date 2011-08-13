3d5a1cd2d08aa2b798d00fde13b05bbb23953e83
mhb0925 <muhaibolove@gmail.com>
2011-08-13 12:10:16 +0800

    优化了案例页界面(show)

--- app/controllers/kases_controller.rb
+++ app/controllers/kases_controller.rb
@@ -1,19 +1,17 @@
 class KasesController < ApplicationController
   respond_to :html
-  before_filter :login_required, :except => [:index]
+  before_filter :login_required, :except => [:index, :show]
+  before_filter :find_problem
   
   def index
-    @problem = Problem.find(params[:problem_id])
     @kases = @problem.kases
   end
   
   def new
-    @problem = Problem.find(params[:problem_id])
     @kase = @problem.kases.build
   end
   
   def create
-    @problem = Problem.find(params[:problem_id])
     @kase = @problem.kases.build(params[:kase])
     @kase.init_geocodding
     if @kase.save
@@ -25,6 +23,18 @@ class KasesController < ApplicationController
   
   def show
     @kase = Kase.find(params[:id])
+    problem_kase_ids = @problem.kases.map(&:id)
+    i = problem_kase_ids.index(@kase.id)
+    if i != @problem.kases.length - 1
+      @nextp = Kase.find(@problem.kases[i+1])
+    else
+      @nextp = Kase.find(@problem.kases[0])
+    end
     @comments = @kase.comments
+  end
+  
+  private
+  def find_problem
+    @problem = Problem.find(params[:problem_id])
   end
 end
