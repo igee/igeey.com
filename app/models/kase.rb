36f88cd8955f56eb260f6707db52d96e5c01f80f
loddit <loddit@gmail.com>
2011-08-16 16:16:04 +0800

    problem icon

--- app/models/kase.rb
+++ app/models/kase.rb
@@ -4,13 +4,14 @@ class Kase < ActiveRecord::Base
   belongs_to :problem
   has_many   :comments, :as => :commentable, :dependent => :destroy
   has_many   :notifications, :as => :notifiable, :dependent => :destroy
+  has_many   :votes,    :as => :voteable,    :dependent => :destroy
   
   has_attached_file :photo, :styles => {:_170x127 => ["170x127#"],:_100x75=>["100x75#"],:_360x270 => ["360x270#"],:max500x400 => ["500x400>"]},
                             :url=>"/media/:attachment/:id/:style.:extension",
                             :default_style=> :_90x64,
                             :default_url=>"/defaults/:attachment/:style.png"
                             
-  default_scope     :order => 'created_at desc'
+  default_scope     :order => 'votes_count desc'
   
   validates :photo_file_name, :presence=>true, :format=>{ :with=>/([\w-]+\.(gif|png|jpg))|/ }
   
--- db/schema.rb
+++ db/schema.rb
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "comments_count",                     :default => 0
+    t.integer  "votes_count",          :default => 0
     t.integer  "last_replied_user_id"
     t.datetime "last_replied_at"
   end
