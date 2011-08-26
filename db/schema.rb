47cac9e00a983e65ce6b4d16bb61554aec35aa71
mhb0925 <muhaibolove@gmail.com>
2011-08-26 12:29:03 +0800

    solution增加了投票

--- db/schema.rb
+++ db/schema.rb
@@ -282,7 +282,8 @@ ActiveRecord::Schema.define do
     t.integer  "problem_id"
     t.string   "title"
     t.text     "content"
-    t.integer  "comments_count",                     :default => 0
+    t.integer  "comments_count",       :default => 0
+    t.integer  "votes_count",          :default => 0
     t.datetime "last_replied_at"
     t.integer  "last_replied_user_id"
     t.datetime "created_at"
